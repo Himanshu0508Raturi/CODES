01b53c1e079713916f2fa2a8cf09abb708d2d38f
Himanshu_Raturi <raturihimanshu077@gmail.com>
2025-02-25 20:07:49 +0530

    new , Q10 , Q11

---
+++ Java/Himanshu_Raturi/Q10_word_example.java
@@ -0,0 +1,79 @@
+package CODES.Java.Himanshu_Raturi;
+import java.util.Scanner;
+public class Q10_word_example {
+
+  private String strdata;
+
+  public Q10_word_example(String str)
+  {
+    
+    int l = str.length();
+    if(str.charAt(l - 1) == '.' || str.charAt(l - 1) == '?' || str.charAt(l - 1) == '!')
+    {
+        strdata = str;
+    }else
+    {
+        System.out.println("Enter a valid string ending with ? , . , !.");
+    }
+  }
+  boolean isVowel(String word)
+  {
+    if ((word.charAt(0) == 'A' || word.charAt(0) == 'E' || word.charAt(0) == 'I' 
+     || word.charAt(0) == 'O' || word.charAt(0) == 'U') 
+    && 
+    (word.charAt(word.length() - 1) == 'A' || word.charAt(word.length() - 1) == 'E' 
+     || word.charAt(word.length() - 1) == 'I' || word.charAt(word.length() - 1) == 'O' 
+     || word.charAt(word.length() - 1) == 'U')) 
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+  }
+  void countword()
+  {
+    int count = 0;
+    String[] token = strdata.trim().split("\\s+"); 
+    for(String word : token)
+    {
+        if (isVowel(word)) 
+        {
+            count++;
+        }
+    
+    }
+    System.out.println("Words starting and ending with Vowels are: " + count);
+  }
+  void placeWord()
+  {
+    String[] token = strdata.trim().split("\\s+");
+    StringBuilder vowelWords = new StringBuilder();
+    StringBuilder otherWords = new StringBuilder();
+    for(String word : token)
+    {
+        if (isVowel(word)) 
+        {
+            vowelWords.append(word + " ");
+        }else
+        {
+            otherWords.append(word + " ");
+        }
+    }
+    System.out.println("Rearranged string.");
+    System.out.print(vowelWords);
+    System.out.println(otherWords + " ");
+
+  }
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the sentence (UPPERCASE and ends with ., ?, or !):");
+    String str;
+    str = sc.nextLine();
+    Q10_word_example obj = new Q10_word_example(str);
+    obj.countword();
+    obj.placeWord();
+    sc.close();
+  }
+}
---
+++ Java/Himanshu_Raturi/Q11_ArrayDemo.java
@@ -0,0 +1,97 @@
+package CODES.Java.Himanshu_Raturi;
+import java.util.Scanner;
+public class Q11_ArrayDemo  {
+        void arrayFunc(int arr[] , int key)
+        {
+            int l = arr.length;
+            System.out.println("Pair of element whose sum is " + key + " are:");
+            for(int i = 0 ; i < l ; i++)
+            {
+                for(int j = i+1 ; j < l ; j++)
+                {
+                    int sum = arr[i] + arr[j];
+                    if(sum == key)
+                    {
+                        System.out.println((arr[i]) +  " + " + (arr[j]) + " = " + key);
+                    }
+                }
+            }
+        }
+        void arrayFunc(int A[] , int p , int B[] , int q)
+        {
+            int temp[] = new int[p+q];
+            int i = 0 , j = 0 , k = 0;
+            while(i < p && j < q)
+            {
+                if(A[i] < B[j])
+                {
+                    temp[k++] = A[i++];
+                }else
+                {
+                    temp[k++] = B[j++];
+                }
+            }
+            while(i < p)
+            {
+                temp[k++] = A[i++];
+            }
+            while(j < q)
+            {
+                temp[k++] = A[i++];
+            }
+            k=0;
+            for(int x = 0 ; x < p ; x++)
+            {
+                A[x] = temp[k++];
+            }
+            for(int y = 0 ; y < q ; y++)
+            {
+                B[y] = temp[k++];
+            }
+            System.out.println("Sorted Arrays: ");
+            System.out.print("A: [");
+            for(int x = 0 ; x < p ; x++)
+            {
+                System.out.print(A[x] + ", ");
+                if(x == p-1)
+                {
+                    System.out.println("]" + "");
+                }
+            }
+            System.out.print("B: [");
+            for(int y = 0 ; y < q ; y++)
+            {
+                System.out.print(B[y] + ", ");
+                if(y == q-1)
+                {
+                    System.out.println("]" + "");
+                }
+            }
+
+        }
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int p , q ; 
+        System.out.print("Enter size of Array A[] and B[]: ");
+        p = sc.nextInt();
+        q = sc.nextInt();
+        int A[] = new int[p];
+        int B[] = new int[q];
+        System.out.print("Enter element in Array A[]: ");
+        for(int i = 0 ; i < p ; i++)
+        {
+            A[i] = sc.nextInt();
+        }
+        System.out.print("Enter element in Array B[]: ");
+        for(int i = 0 ; i < q ; i++)
+        {
+            B[i] = sc.nextInt();
+        }
+        Q11_ArrayDemo obj = new Q11_ArrayDemo();
+        obj.arrayFunc(A, 10);
+        obj.arrayFunc(A, p , B , q);
+        sc.close();
+    }
+}
